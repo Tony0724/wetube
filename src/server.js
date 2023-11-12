9fd7aaed806387a2c4fd60dbf45785899fc9a3ba
sungminkim <0724ksm@naver.com>
2023-11-12 19:07:25 +0900

    made middleware and understanding middleware.

--- src/server.js
+++ src/server.js
@@ -4,9 +4,21 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.")
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next();
+}
 
-app.get("/", handleHome)
+const handleHome = (req, res) => {
+    return res.send("<h1>I understand your request!😄</h1>");
+}
+const handleLogin = (req, res) => {
+    return res.send({message: "Login here."})
+}
+
+app.use(logger);
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
