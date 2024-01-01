045880b6a6acac7f0af3877f8197825e2653814f
sungminkim <0724ksm@naver.com>
2024-01-01 21:34:36 +0900

    make routers.

--- src/server.js
+++ src/server.js
@@ -1,22 +1,39 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const PORT = 4000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`)
-    next();
-}
+const globalRouter = express.Router();
 
-const handleHome = (req, res) => {
-    return res.send("<h1>I understand your request!😄</h1>");
-}
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome)
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User")
+
+userRouter.get("/edit", handleEditUser)
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo)
+
 const handleLogin = (req, res) => {
     return res.send({message: "Login here."})
 }
 
-app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+
+app.use(morgan("dev"));
 app.get("/", handleHome);
 app.get("/login", handleLogin);
 
