7d9baa6146af30db6430f69ee568377b690b6d9c
sungminkim <0724ksm@naver.com>
2024-01-02 17:27:16 +0900

    clean the code.

--- src/server.js
+++ src/server.js
@@ -1,41 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
+import globalRouter from '../routers/globalRouter';
+import videoRouter from '../routers/videoRouter';
+import userRouter from '../routers/userRouter';
 
 const PORT = 4000;
 
 const app = express();
-
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome)
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User")
-
-userRouter.get("/edit", handleEditUser)
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo)
-
-const handleLogin = (req, res) => {
-    return res.send({message: "Login here."})
-}
-
+const logger = morgan("dev")
+app.use(logger)
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
-
-
-app.use(morgan("dev"));
-app.get("/", handleHome);
-app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
