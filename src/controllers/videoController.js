e1b5ce854734b394c87f3d61c2e180b674455233
sungminkim <0724ksm@naver.com>
2024-03-02 21:37:33 +0900

    planning routes.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,6 @@
 export const trending = (req, res) => res.send("Home Page videos");
-
-export const watch = (req, res) => res.send("watch");
-
+export const see = (req, res) => res.send("see");
 export const edit = (req, res) => res.send("Edit")
+export const search = (req, res) => res.send("search");
+export const upload = (req, res) => res.send("upload");
+export const deleteVideo = (req, res) => res.send("Delete Video")
