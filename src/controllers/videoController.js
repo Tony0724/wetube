4b73347d07c6580621d1110327c1bc48aaeeb9da
Tony0724 <0724ksm@naver.com>
2025-01-29 19:58:43 +0900

    URL PARAMETERS 2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,12 @@
 export const trending = (req, res) => res.send("Home Page videos");
 export const see = (req, res) => {
-    console.log(req.params)
-    res.send("Watch");
+    return res.send(`Watch Video #${req.params.id}`);
 }
 export const edit = (req, res) => {
-    console.log(req.params)
-    res.send("Edit")
+    return res.send("Edit")
 }
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => {
-    console.log(req.params)
-    res.send("Delete Video")
+    return res.send("Delete Video")
 }
