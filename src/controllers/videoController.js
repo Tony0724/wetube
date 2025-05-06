61986e485e9ca43b2440ce8f8d6bd8a7c2949a66
Tony0724 <0724ksm@naver.com>
2025-05-06 20:08:16 +0900

    variables to Templates.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch Video"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit Video"});
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => {
