2337754b3c328ca6c0730423e2aa522530aa3cd3
Tony0724 <0724ksm@naver.com>
2025-06-02 16:00:30 +0900

    add favicon.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,8 +4,8 @@ const fakeUser = {
 }
 
 export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser});
-export const see = (req, res) => res.render("watch", {pageTitle: "Watch Video"});
-export const edit = (req, res) => res.render("edit", {pageTitle: "Edit Video"});
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => {
