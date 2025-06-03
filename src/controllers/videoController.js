bfa281dd249f714253a087a63440015d1f76952c
Tony0724 <0724ksm@naver.com>
2025-06-03 10:04:39 +0900

    fixing about problem that are created by fakeUser constant.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,17 @@
-const fakeUser = {
-    username:"sungmin",
-    loggedIn: false,
-}
+export const trending = (req, res) => {
+    const fakeUser = {
+        username:"sungmin",
+        loggedIn: false,
+    }
 
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser});
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+    return res.render("home", {pageTitle: "Home", fakeUser});
+}
+export const see = (req, res) => res.render("watch", {pageTitle: "Home", fakeUser});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Home", fakeUser});
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => {
     return res.send("Delete Video")
 }
+
+// fakeuser를 전역 상수로 놓아야 할 듯..
